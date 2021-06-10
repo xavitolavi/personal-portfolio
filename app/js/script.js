13ca7e3a70a4d54d0bb0756e0158b9c9497239b6
XaviFlorit <33329893+xavitolavi@users.noreply.github.com>
2021-06-10 14:28:05 +0200

    Still in development, missing footer and media queries
    Still in development, missing footer and media queries, after that I'll have the base, I should tweak and change a lot of things.

--- app/js/script.js
+++ app/js/script.js
@@ -68,10 +68,45 @@ tabs.forEach(tab => {
 })
 
 /*==================== SERVICES MODAL ====================*/
+const modalViews = document.querySelectorAll('.services__modal');
+const modalBtns = document.querySelectorAll('.services__button');
+const modalCloses = document.querySelectorAll('.services__modal-close');
 
+let modal = function(modalClick) {
+    modalViews[modalClick].classList.add('active-modal');
+}
 
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () => {
+        modal(i)
+    })
+})
+
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click', () => {
+        modalViews.forEach((modalView) => {
+            modalView.classList.remove('active-modal')
+        })
+    })
+})
 /*==================== PORTFOLIO SWIPER  ====================*/
+var swiper = new Swiper('.portfolio__container', {
+    cssMode: true,
+    loop: true,
 
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    mousewheel: {
+        forceToAxis: true,
+    },
+
+  });
 
 /*==================== TESTIMONIAL ====================*/
 
